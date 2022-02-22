8928d77b800e19113213c694ac7cf1a716fda04d
surindermdb <surinderkumar.mdb@gmail.com>
2022-02-22 15:46:00 +0530

    initial commit

--- dbconfig.js
+++ dbconfig.js
@@ -1,10 +1,4 @@
 const  config = {
-    user:  'postgres', // sql user
-    password:  '123456', //sql user password
-    server:  'localhost', // if it does not work try- localhost
-    database:  'marketplace',
-    port:  5432,
-
     smtp_host : 'smtp.gmail.com',
     smtp_port : '587',
     smtp_user : 'surinderdhanjufss@gmail.com',
